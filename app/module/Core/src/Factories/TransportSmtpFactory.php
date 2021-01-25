2d5d54b5d429b81d40942fff270558b5b24bdbe0
claudio-code <suporte2424@outlook.com>
2021-01-25 03:28:18 -0300

    created factory for smtp

---
+++ app/module/Core/src/Factories/TransportSmtpFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Core\Factories;
+
+use Laminas\Mail\Transport\SmtpOptions;
+use Laminas\Mail\Transport\Smtp as SmtpTransport;
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+
+class TransportSmtpFactory implements FactoryInterface
+{
+  public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+  {
+    $config = $container->get('config');
+    
+    $transport = new SmtpTransport();
+    $options = new SmtpOptions($config['mail']);
+    $transport->setOptions($options);
+
+    return $transport;
+  }
+}
