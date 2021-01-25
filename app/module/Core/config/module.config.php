757874e48fba7334ca7a66eba271fd9058df5c03
claudio-code <suporte2424@outlook.com>
2021-01-25 03:56:22 -0300

    created form factory

--- app/module/Core/config/module.config.php
+++ app/module/Core/config/module.config.php
@@ -1,11 +1,18 @@
 <?php
 
 use Core\Factories\TransportSmtpFactory;
+use Core\Factories\FormElementFactory;
+use Laminas\Form\View\Helper\FormElementErrors;
 
 return [
   'service_manager' => [
     'factories' => [
       'core.transport.smtp' => TransportSmtpFactory::class
     ]
-  ]
+    ],
+    'view_helpers' => [
+      'factories' => [
+        FormElementErrors::class => FormElementErrors::class
+      ]
+    ]
 ];
---
+++ app/module/Core/src/Factories/FormElementFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Core\Factories;
+
+use Interop\Container\ContainerInterface;
+use Laminas\Form\View\Helper\FormElementErrors;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+
+class FormElementFactory implements FactoryInterface
+{
+  public function __invoke(ContainerInterface $container, $requestedName, array $options = null): FormElementErrors
+  {
+    $helper = new FormElementErrors();
+    $config = $container->get('config');
+
+    if (!isset($config['view_helper_config']['form_element_errors'])) {
+      return $helper;
+    }
+
+    $configHelper = $config['view_helper_config']['form_element_errors'];
+    if (isset($configHelper['message_open_format'])) {
+      $helper->setMessageOpenFormat($configHelper['message_open_format']);
+    }
+    
+    if (isset($configHelper['message_separator_string'])) {
+      $helper->setMessageSeparatorString($configHelper['message_separator_string']);
+    }
+    
+    if (isset($configHelper['message_close_string'])) {
+      $helper->setMessageSeparatorString($configHelper['message_close_string']);
+    }
+
+    return $helper;
+  }
+}
